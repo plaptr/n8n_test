7f7f1b2e711b8eecf0e4a7a66f4302018e1bc075
ptrpla <paolopetralia2@gmail.com>
2025-10-23 01:07:58 +0200

    final test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import logo from "./logo.svg";
 import reactLogo from "./react.svg";
 
 export function App() {
-  return <h1>Autodeploy test</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-8xl font-bold">Autodeploy test</h1>
+      <p>This is a test</p>
+    </div>
+  );
 }
 
 export default App;
