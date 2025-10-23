4e3e5a5ef73708aa50770090fcb0c275bb5c2d47
ptrpla <paolopetralia2@gmail.com>
2025-10-23 13:52:18 +0200

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export function App() {
   return (
     <div className="flex flex-col h-screen items-center justify-center gap-2">
       <h1 className="text-5xl font-bold">AutoDeploy</h1>
-      <p>Push -&gt; Deploy test</p>
+      <p>Push -&gt; Deploy</p>
     </div>
   );
 }
