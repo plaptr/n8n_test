af14e2fc04223efc5f62a49a9fd52c06a58e8873
ptrpla <paolopetralia2@gmail.com>
2025-10-23 08:54:33 +0200

    boxshadow

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import reactLogo from "./react.svg";
 
 export function App() {
   return (
-    <div className="flex flex-col h-screen items-center justify-center shadow-lg rounded-sm">
-      <h1 className="text-5xl font-bold">AutoDeploy</h1>
-      <p>Push -&gt; Deploy</p>
+    <div className="flex flex-col h-screen items-center justify-center ">
+      <div className="shadow-lg rounded-sm flex flex-col gap-2 items-center justify-center">
+        <h1 className="text-5xl font-bold">AutoDeploy</h1>
+        <p>Push -&gt; Deploy</p>
+      </div>
     </div>
   );
 }
