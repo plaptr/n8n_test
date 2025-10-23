bf51bc2a016ef5ff266809ed4663d1e83ef9e02b
ptrpla <paolopetralia2@gmail.com>
2025-10-23 14:14:53 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import reactLogo from "./react.svg";
 
 export function App() {
   return (
-    <div className="flex flex-col h-screen items-center justify-center gap-2">
+    <div className="flex flex-col text-red-500 h-screen items-center justify-center gap-2">
       <h1 className="text-5xl font-bold">AutoDeploy</h1>
       <p>Push -&gt; Deploy</p>
     </div>
